83dc770662961e2d4af3993160c21bba9589cb62
wajid01 <wajiullah01@gmail.com>
2025-09-14 21:39:23 +0300

    Updated the Draw file

--- Draw.py
+++ Draw.py
@@ -6,11 +6,13 @@ cv.imshow('cat',img)
 
 #Paint a image on a certain color 
 
-img[100:100, 100:100] = 0,200,0  
-cv.imshow('Neo Img',img) 
+img[50:50, 50:50] = 0,200,0  
+cv.imshow('Neo Img',img)  
 
+#Drawing a Rectangle  
 
-
+cv.rectangle (img,(0,0), (img.shape[1]//2, img.shape[0]//2) ,(0,0,255),thickness=-1) 
+cv.imshow('Rectangle',img)    
 
 
 cv.waitKey(0) 
