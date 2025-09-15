0f9b04df988a19b64f2395a85e2181eca0566d4e
wajid01 <wajiullah01@gmail.com>
2025-09-15 22:38:58 +0300

    Added basic functions file

---
+++ BasicFunctions.py
@@ -0,0 +1,10 @@
+import cv2 as cv 
+
+img =  cv.imread('Photos/cat.jpg') 
+cv.imshow('Cat',img) 
+
+
+
+
+cv.waitKey(0) 
+
--- Draw.py
+++ Draw.py
@@ -12,7 +12,19 @@ cv.imshow('Neo Img',img)
 #Drawing a Rectangle  
 
 cv.rectangle (img,(0,0), (img.shape[1]//2, img.shape[0]//2) ,(0,0,255),thickness=-1) 
-cv.imshow('Rectangle',img)    
+cv.imshow('Rectangle',img)     
+
+#Drawing a Circle 0,
+cv.circle(img,(img.shape[1]//2, img.shape[0]//2),40,(0,0,255) , thickness= -1) 
+cv.imshow('Circle',img) 
+
+#Drawing a Line 
+cv.line(img,(0,0) , (img.shape[1]//2, img.shape[0]//2), (255,255,255),thickness= 3) 
+cv.imshow('Line',img)  
+
+#writing Text 
+cv.putText(img, 'Hello' ,(255,255), cv.FONT_HERSHEY_COMPLEX, 2.0, (0,255,0),2)  
+cv.imshow('Text',img)
 
 
 cv.waitKey(0) 
